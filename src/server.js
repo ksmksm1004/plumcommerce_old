309f6cee32fe0d425540e0a309bc46da41b29927
ksmksm1004 <ksmksm1004@gmail.com>
2026-06-13 14:26:50 +0000

    cache

--- src/server.js
+++ src/server.js
@@ -16,7 +16,13 @@ app.enable('trust proxy');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
-app.use('/image', express.static(path.resolve('./public/image')));
+// maxAge와 setHeaders 옵션을 추가하여 백엔드가 이미지를 넘길 때 캐시 보증서를 강제로 붙이게 합니다.
+app.use('/image', express.static(path.resolve('./public/image'), {
+  maxAge: '1d', // 🌟 일주일 또는 하루(1d) 동안 브라우저/CDN 캐싱 허용
+  setHeaders: (res, path, stat) => {
+    res.set('Cache-Control', 'public, max-age=86400'); // CloudFront가 흡수할 수 있도록 public 마크 강제 주입
+  }
+}));
 app.use('/styles', express.static(path.resolve('./public/styles')));
 app.use('/assets', express.static(path.resolve('./public/assets')));
 
